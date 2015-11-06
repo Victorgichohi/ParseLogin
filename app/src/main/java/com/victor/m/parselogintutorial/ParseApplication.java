781861e2061bc7ffcd041fc62ed0f9b4d6ca8ba4
victorgichohi <victorgichohi48@gmail.com>
2015-11-06 17:52:51 +0300

    new java class,ParseApplication with the application id and key

---
+++ app/src/main/java/com/victor/m/parselogintutorial/ParseApplication.java
@@ -0,0 +1,32 @@
+package com.victor.m.parselogintutorial;
+
+/**
+ * Created by victor on 11/6/15.
+ */
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+import com.parse.ParseUser;
+
+import android.app.Application;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Add your initialization code here
+        Parse.initialize(this, jh5Sf1tioKsMd2HJKanNybyHQeZvtEx5PmRYfXN9, mbEejIkVJ0ewCezlJcGZbFSuOb6FeiQIsXDCPPac);
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+        // If you would like all objects to be private by default, remove this
+        // line.
+        defaultACL.setPublicReadAccess(true);
+
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+
+}
