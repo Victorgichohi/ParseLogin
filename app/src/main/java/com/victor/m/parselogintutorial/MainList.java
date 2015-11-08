6517b4eed16aafe8d6a7328d724e399b00f79327
victorgichohi <victorgichohi48@gmail.com>
2015-11-08 22:18:33 +0300

    changed the text displayed on the list

--- app/src/main/java/com/victor/m/parselogintutorial/MainList.java
+++ app/src/main/java/com/victor/m/parselogintutorial/MainList.java
@@ -104,34 +104,30 @@ public class MainList extends Activity {
         listDataChild = new HashMap<String, List<String>>();
 
         // Adding child data
-        listDataHeader.add("Top 250");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
+        listDataHeader.add("Select Your Parking Ticket");
+        listDataHeader.add("Select Your closest area");
+        listDataHeader.add("Select Your vehicle type");
 
         // Adding child data
         List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
+        top250.add("Daily");
+        top250.add("Weekly(10% off");
+        top250.add("Monthly (20% off)");
+
 
         List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
+        nowShowing.add("CBD");
+        nowShowing.add("West Lands");
+        nowShowing.add("Ngong road");
+        nowShowing.add("TomBoya");
+
 
         List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
+        comingSoon.add("Toyota");
+        comingSoon.add("Bmw");
+        comingSoon.add("RangeRover");
+        comingSoon.add("Mercedes");
+        comingSoon.add("Volkswagen");
 
         listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
         listDataChild.put(listDataHeader.get(1), nowShowing);
