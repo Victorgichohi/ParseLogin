6822fe9cd8077ff9be086798905dd8b02178d11c
victorgichohi <victorgichohi48@gmail.com>
2015-11-10 10:43:39 +0300

    all activities complete for now except timer

--- app/src/main/java/com/victor/m/parselogintutorial/Welcome.java
+++ app/src/main/java/com/victor/m/parselogintutorial/Welcome.java
@@ -17,7 +17,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.parse.ParseUser;
 
@@ -34,16 +33,16 @@ public class Welcome extends Activity {
         setContentView(R.layout.welcome);
 
         // Retrieve current user from Parse.com
-        ParseUser currentUser = ParseUser.getCurrentUser();
+//        ParseUser currentUser = ParseUser.getCurrentUser();
 
         // Convert currentUser into String
-        String struser = currentUser.getUsername().toString();
+//        String struser = currentUser.getUsername().toString();
 
         // Locate TextView in welcome.xml
-        TextView txtuser = (TextView) findViewById(R.id.txtuser);
+//        TextView txtuser = (TextView) findViewById(R.id.txtuser);
 
         // Set the currentUser String into TextView
-        txtuser.setText("You are logged in as " + struser);
+//        txtuser.setText("You are logged in as " + struser);
 
         // Locate Button in welcome.xml
         logout = (Button) findViewById(R.id.logout);
@@ -53,7 +52,9 @@ public class Welcome extends Activity {
 
             public void onClick(View arg0) {
                 // Logout current user
+                Intent lognje = new Intent(Welcome.this,LoginSignupActivity.class);
                 ParseUser.logOut();
+                startActivity(lognje);
                 finish();
             }
 
