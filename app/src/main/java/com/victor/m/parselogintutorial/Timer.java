98d5286cbb0f11fc8657cf2be2262a19f0119e71
victorgichohi <victorgichohi48@gmail.com>
2015-11-10 10:45:38 +0300

    timer java file complete

--- app/src/main/java/com/victor/m/parselogintutorial/Timer.java
+++ app/src/main/java/com/victor/m/parselogintutorial/Timer.java
@@ -1,29 +1,87 @@
 package com.victor.m.parselogintutorial;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
 
-public class Timer extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Timer extends Activity {
+
+    private TextView txtTimerDay, txtTimerHour, txtTimerMinute, txtTimerSecond;
+    private TextView tvEvent;
+    private Handler handler;
+    private Runnable runnable;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timer);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        txtTimerDay = (TextView) findViewById(R.id.txtTimerDay);
+        txtTimerHour = (TextView) findViewById(R.id.txtTimerHour);
+        txtTimerMinute = (TextView) findViewById(R.id.txtTimerMinute);
+        txtTimerSecond = (TextView) findViewById(R.id.txtTimerSecond);
+        tvEvent = (TextView) findViewById(R.id.tvhappyevent);
+
+        countDownStart();
+
     }
+
+    public void countDownStart() {
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                handler.postDelayed(this, 1000);
+                try {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat(
+                            "yyyy-MM-dd");
+                    // Please here set your event date//YYYY-MM-DD
+                    Date futureDate = dateFormat.parse("2015-11-25");
+                    Date currentDate = new Date();
+                    if (!currentDate.after(futureDate)) {
+                        long diff = futureDate.getTime()
+                                - currentDate.getTime();
+                        long days = diff / (24 * 60 * 60 * 1000);
+                        diff -= days * (24 * 60 * 60 * 1000);
+                        long hours = diff / (60 * 60 * 1000);
+                        diff -= hours * (60 * 60 * 1000);
+                        long minutes = diff / (60 * 1000);
+                        diff -= minutes * (60 * 1000);
+                        long seconds = diff / 1000;
+                        txtTimerDay.setText("" + String.format("%02d", days));
+                        txtTimerHour.setText("" + String.format("%02d", hours));
+                        txtTimerMinute.setText(""
+                                + String.format("%02d", minutes));
+                        txtTimerSecond.setText(""
+                                + String.format("%02d", seconds));
+                    } else {
+                        tvEvent.setVisibility(View.VISIBLE);
+                        tvEvent.setText("Start event now!");
+                        textViewGone();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        handler.postDelayed(runnable, 1 * 1000);
+    }
+
+    public void textViewGone() {
+        findViewById(R.id.txtTimerDay).setVisibility(View.GONE);
+        findViewById(R.id.txt_TimerDay).setVisibility(View.GONE);
+        findViewById(R.id.txtTimerHour).setVisibility(View.GONE);
+        findViewById(R.id.txt_TimerHour).setVisibility(View.GONE);
+        findViewById(R.id.txtTimerMinute).setVisibility(View.GONE);
+        findViewById(R.id.txt_TimerMinute).setVisibility(View.GONE);
+        findViewById(R.id.txtTimerSecond).setVisibility(View.GONE);
+        findViewById(R.id.txt_TimerSecond).setVisibility(View.GONE);
+    }
+
 
 }
